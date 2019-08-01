dddf155450a3b9f15414ab9355440a017ad477aa
tanlianwang <tanlianwang@jimimax.com>
2019-08-01 09:00:53 +0800

    【增加代码】 signTest测试类

---
+++ src/main/java/com/jimi/MD5/SingTest.java
@@ -0,0 +1,52 @@
+/*
+ * COPYRIGHT. ShenZhen JiMi Technology Co., Ltd. 2019.
+ * ALL RIGHTS RESERVED.
+ *
+ * No part of this publication may be reproduced, stored in a retrieval system, or transmitted,
+ * on any form or by any means, electronic, mechanical, photocopying, recording,
+ * or otherwise, without the prior written permission of ShenZhen JiMi Network Technology Co., Ltd.
+ *
+ * Amendment History:
+ *
+ * Date                   By              Description
+ * -------------------    -----------     -------------------------------------------
+ * 2019/7/23    TanLianWang        Create the class
+ * http://www.jimilab.com/
+ */
+
+
+package com.jimi.MD5;
+
+import java.util.*;
+
+/**
+ * @author TanLianWang
+ * @version 1.0
+ * @ProjectName demo
+ * @Description
+ * @Date 2019/7/23 16:58
+ */
+public class SingTest {
+    public static String putPairsSequenceAndTogether(Map<String, String> info) {
+        List<Map.Entry<String, String>> infoIds = new ArrayList<Map.Entry<String, String>>(info.entrySet());
+        Collections.sort(infoIds, new Comparator<Map.Entry<String, String>>() {
+            @Override
+            public int compare(Map.Entry<String, String> arg0, Map.Entry<String, String> arg1) {
+                return (arg0.getKey()).compareTo(arg1.getKey());
+            }
+        });
+        String ret = "";
+        for (Map.Entry<String, String> entry : infoIds) {
+            ret += entry.getKey();
+            ret += "=";
+            ret += entry.getValue();
+            ret += "&";
+        }
+        ret = ret.substring(0, ret.length() - 1);
+        return ret;
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/main/java/com/jimi/MD5/slat.java
+++ src/main/java/com/jimi/MD5/slat.java
@@ -17,8 +17,10 @@
 
 package com.jimi.MD5;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.SimpleHash;
+import sun.security.provider.MD5;
 
 import java.util.ArrayList;
 import java.util.Vector;
@@ -30,28 +32,43 @@ import java.util.Vector;
  */
 public class slat {
 
-        public static void main(String[] args) {
-            String password = "321";
-            String salt = new SecureRandomNumberGenerator().nextBytes().toString();
-            int times = 8;
-            String algorithmName = "md5";
+    public static void main(String[] args) {
+        String password = "321";
+        String salt = new SecureRandomNumberGenerator().nextBytes().toString();
+        int times = 8;
+        String algorithmName = "md5";
 
-            String encodedPassword = new SimpleHash(algorithmName,password,salt,times).toString();
+        String encodeStr;
+        System.out.println(MD5("t19962864",1));
 
-            System.out.printf("原始密码是 %s , 盐是： %s, 运算次数是： %d, 运算出来的密文是：%s ",password,salt,times,encodedPassword);
 
-            Object testObject = new ArrayList();
-            displayObjectClass(testObject);
-        }
+
+
+        String encodedPassword = new SimpleHash(algorithmName, password, salt, times).toString();
+
+        System.out.printf("原始密码是 %s , 盐是： %s, 运算次数是： %d, 运算出来的密文是：%s ", password, salt, times, encodedPassword);
+
+        Object testObject = new ArrayList();
+        displayObjectClass(testObject);
+    }
+
     public static void displayObjectClass(Object o) {
         if (o instanceof Vector) {
             System.out.println("对象是 java.util.Vector 类的实例");
-        }else if (o instanceof ArrayList) {
+        } else if (o instanceof ArrayList) {
             System.out.println("对象是 java.util.ArrayList 类的实例");
-        }
-        else {
+        } else {
             System.out.println("对象是 " + o.getClass() + " 类的实例");
         }
+    }
+
+    public static String MD5 (String s,Integer times){
+
+        for( int i = 0;i < times; i++){
+           s = DigestUtils.md5Hex(s);
+        }
+        return  s;
+
     }
 }
 
