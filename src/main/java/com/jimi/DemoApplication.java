fdf788baf800e12ab2899c80abdd4246c14101da
ttanyan <1075379406@qq.com>
2020-12-23 00:38:34 +0800

    【add】NaCosConfig:增加nacosconfig配置管理

--- src/main/java/com/jimi/DemoApplication.java
+++ src/main/java/com/jimi/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.jimi;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
  * @date 2019/1/15 15:06
  */
 @SpringBootApplication
+@NacosPropertySource(dataId = "demo", autoRefreshed = true)
 public class DemoApplication {
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
     public static void main(String[] args) {
---
+++ src/main/java/com/jimi/nacoscontorller/ConfigController.java
@@ -0,0 +1,48 @@
+/*
+ * COPYRIGHT. ShenZhen JiMi Technology Co., Ltd. 2020.
+ * ALL RIGHTS RESERVED.
+ *
+ * No part of this publication may be reproduced, stored in a retrieval system, or transmitted,
+ * on any form or by any means, electronic, mechanical, photocopying, recording,
+ * or otherwise, without the prior written permission of ShenZhen JiMi Network Technology Co., Ltd.
+ *
+ * Amendment History:
+ *
+ * Date                   By              Description
+ * -------------------    -----------     -------------------------------------------
+ * 2020/12/22    anyant         Create the class
+ * http://www.jimilab.com/
+ */
+
+
+package com.jimi.nacoscontorller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * @author anyant
+ * @version 1.0
+ * @ProjectName demo
+ * @Description
+ * @Date 2020/12/22 下午11:43
+ */
+@Slf4j
+@Controller
+@RequestMapping("/config")
+public class ConfigController {
+    @NacosValue(value = "${useLocalCache}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        log.info("NaCosConfigValue:"+useLocalCache);
+        return useLocalCache;
+    }
+}
